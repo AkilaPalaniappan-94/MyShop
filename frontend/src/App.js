81acb338ab58d02a86aed1a50c60fed7ed24db7f
AkilaPalaniappan-94 <akilapl94@gmail.com>
2025-01-30 18:55:48 -0500

    Header,Footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 const App=()=> {
   return (
     <>
+    <Header/>
     <h1>Welcome</h1>
-    
+    <Footer/>
     </>
   );
 }
